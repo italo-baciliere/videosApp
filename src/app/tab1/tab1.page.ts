82dd202dedf8bf7a21416817100e15fd8ea428a9
italo-baciliere <italoluciano57@gmail.com>
2022-09-10 20:33:19 -0300

    Adicionado o Item Sliding e finalizado a lista de perfis

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page{
+
+  imagem = '/assets/jurica-koletic.jpg';
+  personas = [
+    {
+      nome: 'Bill',
+      url: 'https://www.lance.com.br/files/article_main/uploads/2022/09/07/63189a0c08120.jpeg',
+      municipio: 'Centro de Contagem',
+      quantidadeVagas: 0,
+      score: '3.7'
+    },
+    {
+      nome: 'Karen Lana',
+      url: 'https://assets.website-files.com/5e51c674258ffe10d286d30a/5e5355ed4600809f5a8dad51_peep-37.svg',
+      municipio: 'Centro de Contagem',
+      quantidadeVagas: 2,
+      score: '5.0'
+    },
+    {
+      nome: 'Rodolfo Sales',
+      url: 'https://assets.website-files.com/5e51c674258ffe10d286d30a/5e535da59588e079598123fc_peep-103.svg',
+      municipio: 'Centro de Contagem',
+      quantidadeVagas: 4,
+      score: '4.5'
+    },
+    {
+      nome: 'Marcos Rodarte',
+      url: 'https://assets.website-files.com/5e51c674258ffe10d286d30a/5e532c33f3aa4b390f1bd62f_peep-3.svg',
+      municipio: 'Centro de Contagem',
+      quantidadeVagas: 4,
+      score: '4.0'
+    }
+  ];
 
   constructor() {}
 
